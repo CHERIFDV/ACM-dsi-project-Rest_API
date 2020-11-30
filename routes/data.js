7d31f20d277e1de2b65d120fcde701e10beeda2a
CHERIFDV <47925597+CHERIFDV@users.noreply.github.com>
2020-11-30 18:21:34 +0100

    Add files via upload

---
+++ routes/data.js
@@ -0,0 +1,15 @@
+module.exports={
+    products: [
+      {
+        id: '1',
+        title: 'Slim Shirt',
+        category: 'Shirts',
+        imageurl: 'p1.png',
+        price: 60,
+        backgroundcolor:"red",
+        discount: 4.5,
+        stock: 2,
+        description:"dfgdfgfdgfdgdf"
+      }
+    ]
+  }
